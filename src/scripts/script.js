2a11e5a63f1dbde06f403f6a64f051638cd13f7b
Roche Stapelberg <stapelberg.roche@gmail.com>
2022-05-12 17:04:34 +0200

    Chnage file structure and added sections

---
+++ src/scripts/script.js
@@ -0,0 +1,8 @@
+const hamburgerButton = document.getElementById('hamburger');
+const navList = document.getElementById('nav-list');
+
+function ToggleButton() {
+    navList.classList.toggle('show')
+}
+
+hamburgerButton.addEventListener('click', ToggleButton)
